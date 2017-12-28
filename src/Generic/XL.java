f5d710742871ea64f457420b6d1aa32220eddae2
purnendukumar82 <34840896+purnendukumar82@users.noreply.github.com>
2017-12-28 14:09:32 +0530

    XL file updated

--- src/Generic/XL.java
+++ src/Generic/XL.java
@@ -1,13 +1,36 @@
 package Generic;
 
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class XL {
-	public void getCellValue()
-	{
+	public String getCellValue(String path, String sheet, int r, int c)
+	{   String v1="";
+		try {
+		Workbook wb1 = WorkbookFactory.create(new FileInputStream(path));
+		 v1=wb1.getSheet(sheet).getRow(r).getCell(c).toString();
+	        }
+		catch(Exception e)
+		{
+			
+		}
+		return v1;
 		
 	}
    
-	public void getRowCount()
-	{
-		
+	public int getRowCount(String path,String sheet)
+	{   
+		int rc=0;
+		try {
+		Workbook wb2=WorkbookFactory.create(new FileInputStream(path));
+		rc=wb2.getSheet(sheet).getLastRowNum();
+		}
+		catch(Exception e)
+		{
+			
+		}
+		return rc;
 	}
 }
---
+++ src/Script/testValidLogin.java
@@ -0,0 +1,18 @@
+package Script;
+
+import org.testng.annotations.Test;
+
+import Generic.BaseTest1;
+import Page.LoginPage;
+
+public class testValidLogin extends BaseTest1{
+	@Test(priority=1, groups={"Login"})
+	public void  testValidLogin()
+	{
+	
+		
+	}
+	
+	
+
+}
